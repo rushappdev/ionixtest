d4356e6b4615283fae6a053690cd03ec04d1ba75
Rashaan Thompson <Rashaan Thompson>
2016-11-21 02:10:06 -0500

    new changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -103,8 +103,8 @@ $scope.data = {};
     $scope.submit = function(){
         // var link = 'http://nikola-breznjak.com/_testings/ionicPHP/api.php';
         // var link = 'http://127.0.0.1/main/Source/api.php';
-        // var link = 'http://rawgit.com/rushappdev/myAPis/master/api.php';
-        var link = 'api.php';
+        var link = 'http://rawgit.com/rushappdev/myAPis/master/api.php';
+        // var link = 'api.php';
 
         $http({
             method: 'POST',
